fa44524c21d379c20a8a3333c39fa102107f7a82
Raymond-ss <343383188@tdsb.ca>
2026-05-04 15:00:22 +0000

    Finish 4.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,12 +1,55 @@
+/**
+	* File: Lesson 4.1: While Loops and accumulators
+	* Author: Raymond
+	*	Date Created: April 3, 2023
+	*	Date Last Modified: May 4, 2023
+	*/
+
 public class PracticeProblem {
 
+ 
 	public static void main(String args[]) {
+	
+	}
+	public static String seriesInclusive(int x) { 
+		int num = 1;
+		String numS = "";
+		if (x <= 0) {
+			return("");
+		}
+		while (x >= num) {
+			if (num == x) {					
+				numS += num;
+				return numS;
+			}
+			numS += num + " ";
+			num++;
+		}
+		return numS;
+		}
+		public static String seriesExclusive(int x) { 
+		int num = 0;
+		String numS = "";
+		if (x <= 0) {
+			return("");
+		}
+		while (x > num) {
+			if (num == x) {					
+				numS += num;
+				return numS.trim();
+			}
+			numS += num + " ";
+			num++;
+		}
+		return numS.trim();
+		}
+		
+	
 
+	public static void q1() {	
+		
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
 
 	public static void q2() {
 		//Write question 2 code here
